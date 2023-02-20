40565e8bc95262e687d000e87c32163394830dcf
Bram Janssens <s.a.janssens@gmail.com>
2023-02-20 13:36:53 +0100

    Console app demo created.

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
     void print() {
         System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        String text1 = input.nextLine();
+        String text2 = input.nextLine();
+        String text3 = input.nextLine();
+        String text4 = input.nextLine();
+        System.out.println("Klaar " + text1);
     }
 }
---
+++ src/main/resources/Lab.md
@@ -0,0 +1,9 @@
+## Opdracht
+Maak een console-app die het volgende doet:
+```console
+Welkom!
+Wie ben jij?
+Naam: ............
+Hallo <jenaam>!
+```
+
