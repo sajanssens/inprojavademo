62a9499128af8be3ec8858f87230a65e9802fc9d
Bram Janssens <s.a.janssens@gmail.com>
2023-02-20 13:38:05 +0100

    Cleanup.

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -8,9 +8,6 @@ public class HelloWorld {
         System.out.println("Hello world!");
         Scanner input = new Scanner(System.in);
         String text1 = input.nextLine();
-        String text2 = input.nextLine();
-        String text3 = input.nextLine();
-        String text4 = input.nextLine();
         System.out.println("Klaar " + text1);
     }
 }
