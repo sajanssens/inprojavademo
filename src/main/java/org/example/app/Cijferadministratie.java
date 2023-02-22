fcf5253e051ce8c3905c7bb28c13af7a6b50eaed
Bram Janssens <s.a.janssens@gmail.com>
2023-02-22 14:09:10 +0100

    Cijferadministratie with ArrayList.

--- src/main/java/org/example/app/Cijferadministratie.java
+++ src/main/java/org/example/app/Cijferadministratie.java
@@ -1,5 +1,6 @@
 package org.example.app;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Cijferadministratie {
@@ -10,7 +11,44 @@ public class Cijferadministratie {
         System.out.println("Geef hieronder de cijfers een voor een op, maximaal 100.");
         System.out.println("Sluit af met \"stop\".");
 
+        // arrayvariant();
+
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Float> cijfers = new ArrayList</*Float*/>(); // type inference
+        // generics = "generic types"
+
+        while (true) {
+            String line = scanner.nextLine();
+            if ("stop".equals(line)) break;
+
+            float cijfer = Float.parseFloat(line);
+            cijfers.add(cijfer);
+        }
+
+        float som = 0;
+        for (Float cijfer : cijfers) {
+            som += cijfer;
+        }
+        float gemiddelde = som / cijfers.size();
+
+        // SELECT AVG(waarde)
+        // FROM Cijfers
+        // WHERE waarde > 5.5
+
+        double gemidd = cijfers.stream() // FROM Cijfers
+                .filter(c -> c > 5.5) // WHERE waarde > 5.5
+                .mapToDouble(Float::doubleValue).average() // SELECT AVG(waarde)
+                .orElse(0.0);
+
+        System.out.println("Je staat gemiddeld: " + gemiddelde);
+        System.out.println("Gemiddelde van de voldoendes: " + gemidd);
+
+        System.out.println("Bye bye!");
+    }
+
+    private static void arrayvariant() {
+        Scanner scanner = new Scanner(System.in);
+
         float[] cijfers = new float[100];
 
         String input = "";
@@ -31,6 +69,5 @@ public class Cijferadministratie {
         }
 
         System.out.println("Je staat gemiddeld: " + (totaal / aantal));
-        System.out.println("Bye bye!");
     }
 }
